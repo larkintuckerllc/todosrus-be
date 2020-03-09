612b61e9a8097605e874f8e6aa73cc8220a972f5
John Tucker <john@larkintuckerllc.com>
2020-03-09 16:05:55 -0400

    remove unused script

--- scripts/create-table.sh
+++
@@ -1,5 +0,0 @@
-aws dynamodb create-table \
-    --table-name Todos \
-    --attribute-definitions AttributeName=Id,AttributeType=S \
-    --key-schema AttributeName=Id,KeyType=HASH \
-    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
