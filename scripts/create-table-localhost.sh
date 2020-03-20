332083fed88d0e006f51052933421beb57fcc14d
John Tucker <john@larkintuckerllc.com>
2020-03-20 06:28:44 -0400

    create-table

--- scripts/create-table-localhost.sh
+++ scripts/create-table-localhost.sh
@@ -1,6 +1,10 @@
 aws dynamodb create-table \
     --table-name Todos \
-    --attribute-definitions AttributeName=Id,AttributeType=S \
-    --key-schema AttributeName=Id,KeyType=HASH \
+    --attribute-definitions \
+    AttributeName=IdentityId,AttributeType=S \
+    AttributeName=Id,AttributeType=S \
+    --key-schema \
+    AttributeName=IdentityId,KeyType=HASH \
+    AttributeName=Id,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
     --endpoint-url http://localhost:8000
