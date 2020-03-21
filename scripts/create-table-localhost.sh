df5f4eb43be1dd0fe0c01cf8bb7d0134b547f38b
John Tucker <john@larkintuckerllc.com>
2020-03-21 17:09:45 -0400

    happy path

--- scripts/create-table-localhost.sh
+++ scripts/create-table-localhost.sh
@@ -8,3 +8,12 @@ aws dynamodb create-table \
     AttributeName=Id,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
     --endpoint-url http://localhost:8000
+
+aws dynamodb create-table \
+    --table-name Subscriptions \
+    --attribute-definitions \
+    AttributeName=IdentityId,AttributeType=S \
+    --key-schema \
+    AttributeName=IdentityId,KeyType=HASH \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --endpoint-url http://localhost:8000
